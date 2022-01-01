a2f8fefc810cb2199ab7f8831112c7a5c4449c82
Nenad Stevic <nate@snackbook-bro.home>
2022-01-01 01:57:57 +0100

    finished desktop version of site

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 /*
-**Slideshow script
+**Slideshow script that cycles through images in the Header
 */
 var slideIndex = 0;
 
@@ -8,116 +8,133 @@ carousel();
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
+
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
+
+  if (slideIndex > x.length) {
+    slideIndex = 1
+  }
+
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000);
 }
 
+/*
+**Script that makes Navbar disappear and reaper based on scroll behaviour
+*/
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
+
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
+
   prevScrollpos = currentScrollPos;
 } 
 
-/*
-**Navbar script
-*/
-/*
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+function responsive(window_width) {
+  if(window_width.matche){
+    /*
+    **Script that triggers animations on scroll
+    */
+    animateScroll();
 
-window.onscroll = function() {stick_navbar()};
+    function animateScroll() {
+      var elements;
+      var windowHeight;
 
-function stick_navbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}*/
+      function init() {
+        elements = document.querySelectorAll(".js_scroll, .js_scroll2, .js_scroll3");
+        windowHeight = window.innerHeight;
+      }
 
-/*
-**Scroll animation script
-*/
+      function checkPosition() {
 
-animateScroll();
+        for (var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+          var positionFromTop = elements[i].getBoundingClientRect().top;
 
-function animateScroll() {
-  var elements;
-  var windowHeight;
+          if (positionFromTop - windowHeight <= -120) {
+            element.classList.add('scrolled');
+          }
+        }
+      }
 
-  function init() {
-    elements = document.querySelectorAll(".js_scroll, .js_scroll2, .js_scroll3");
-    windowHeight = window.innerHeight;
-  }
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
 
-  function checkPosition() {
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var positionFromTop = elements[i].getBoundingClientRect().top;
+      init();
+      checkPosition();
+    };
 
-      if (positionFromTop - windowHeight <= -120) {
-      element.classList.add('scrolled');
+    /*
+    **Script that increases video size and autoplays video based on scroll position
+    */
+    scaleVideo();
+
+    function scaleVideo() {
+
+      window.addEventListener('load', function () {
+        var video = document.getElementById('vid'),
+          footerHeight = document.getElementById("page_footer").offsetHeight;
+        docHeight = document.documentElement.offsetHeight - footerHeight;
+
+        window.addEventListener('scroll', function () {
+          // normalize scroll position as percentage
+          var scrolled = window.scrollY / (docHeight - window.innerHeight);
+
+          if (scrolled > 1) scrolled = 1;
+
+          if (scrolled > 0.8) {
+            transformValue = 'scale(' + scrolled + ')';
+            video.play();
+          }
+          else {
+            transformValue = 'scale(0.8)';
+          }
+
+          if (video != null) {
+            video.style.WebkitTransform = transformValue;
+            video.style.MozTransform = transformValue;
+            video.style.OTransform = transformValue;
+            video.style.transform = transformValue;
+          }
+        }, false);
+      }, false);
+    }
+
+    /*
+    **Function that is used by scaleVideo() to autoplay video
+    */
+    function unmuteVideo() {
+      var video = document.getElementById("vid"),
+        mute = document.getElementById("mute");
+
+      if (video.muted == true) {
+        video.muted = false;
+        video.style.filter = "none";
+        mute.style.display = "none";
+      }
+      else {
+        video.muted = true;
+        video.style.filter = "brightness(0.6)";
+        mute.style.display = "block";
       }
     }
   }
-
-  window.addEventListener('scroll', checkPosition);
-  window.addEventListener('resize', init);
-
-  init();
-  checkPosition();
-};
-
-
-window.addEventListener( 'load', function() {
-  var video = document.getElementById('vid'),
-      docHeight = document.documentElement.offsetHeight;
-  
-  window.addEventListener( 'scroll', function() {
-        // normalize scroll position as percentage
-    var scrolled = window.scrollY / ( docHeight - window.innerHeight );
-
-    if(scrolled > 1) scrolled = 1;
-
-    if(scrolled > 0.8) {
-      transformValue = 'scale('+scrolled+')';
-    }
-    else {
-      transformValue = 'scale(0.8)';
-    }
-
-    if(video != null) {
-      video.style.WebkitTransform = transformValue;
-      video.style.MozTransform = transformValue;
-      video.style.OTransform = transformValue;
-      video.style.transform = transformValue;
-    }
-
-    if(scrolled == 1.0) {
-      video.play();
-    }
-    
-  }, false);
-  
-}, false);
-
-function unmuteVideo() {
-  var video = document.getElementById("vid");
-
-  if(video.muted == true) {
-    video.muted = false;
-  }
   else {
-    video.muted = true;
+
   }
 }
+
+var x = window.matchMedia("(min-width: 800px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state ch
